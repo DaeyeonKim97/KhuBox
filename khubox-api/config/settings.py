28197f90bcc66bc381cfc5c4906a5694b1518938
Seungwan Seo <wan9824@naver.com>
2020-06-08 02:32:02 +0900

    feat: create models

--- khubox-api/config/settings.py
+++ khubox-api/config/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['127.0.0.1', 'khubox-api.khunet.net']
 # Application definition
 
 INSTALLED_APPS = [
+    'khubox.apps.KhuboxConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- khubox-api/khubox/models.py
+++ khubox-api/khubox/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
-# Create your models here.
+
+class File(models.Model):
+    id = models.CharField(primary_key=True, max_length=36)
+    parent_id = models.CharField(max_length=36, blank=True, null=True)
+    owner_user_id = models.IntegerField(blank=True, null=True)
+    owner_group_id = models.IntegerField(blank=True, null=True)
+    uploader_id = models.IntegerField(blank=True, null=True)
+    type = models.CharField(max_length=6)
+    name = models.CharField(max_length=255)
+    size = models.BigIntegerField()
+    is_public = models.IntegerField(default=0)
+    is_starred = models.IntegerField(default=0)
+    is_trahsed = models.IntegerField(default=0)
+    created_at = models.DateTimeField()
+    deleted_at = models.DateTimeField(blank=True, null=True)
+
+
+class Group(models.Model):
+    owner_id = models.IntegerField()
+    name = models.CharField(max_length=50)
+    root_folder = models.CharField(max_length=36)
+    invite_code = models.CharField(max_length=36)
+    created_at = models.DateTimeField()
+
+
+class GroupUser(models.Model):
+    group_id = models.IntegerField()
+    user_id = models.IntegerField()
+    joined_at = models.DateTimeField()
+
+
+class User(models.Model):
+    email = models.CharField(max_length=255)
+    password = models.CharField(max_length=60)
+    name = models.CharField(max_length=50)
+    root_folder = models.CharField(max_length=36)
+    created_at = models.DateTimeField()
