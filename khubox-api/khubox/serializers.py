421ba898d1d3b3fcfea7c8651344a31d7f12d135
Seungwan Seo <wan9824@naver.com>
2020-06-10 03:30:31 +0900

    chore: rebase trash, user api

--- khubox-api/khubox/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import File
-
-
-class FileSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = File
-        fields = ['id', 'parent_id', 'owner_id', 'uploader_id', 'type', 'name', 'size', 'is_public', 'is_shared',
-                  'is_starred', 'is_trahsed', 'created_at']
