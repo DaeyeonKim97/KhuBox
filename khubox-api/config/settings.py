a23724ba14a661bfef285f14ab15769a01991628
Seungwan Seo <wan9824@naver.com>
2020-06-17 04:44:27 +0900

    fix: set cors origin allow from all

--- khubox-api/config/settings.py
+++ khubox-api/config/settings.py
@@ -133,8 +133,4 @@ S3_BUCKET = 'khubox-files'
 
 # Cors
 CORS_ALLOW_CREDENTIALS = True
-CORS_ORIGIN_WHITELIST = [
-    'http://localhost:3000',
-    'http://127.0.0.1:3000',
-    'https://khubox.khunet.net',
-]
+CORS_ORIGIN_ALLOW_ALL = True
